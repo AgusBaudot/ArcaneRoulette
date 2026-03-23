b233371c656e7158bacf9d3d5f442acf696a8695
Toto <totolino0411@gmail.com>
2026-03-23 19:07:46 -0300

    [Add] Blackboard

---
+++ Assets/_Scripts/3. World/Enemies/BehaviourTree/Blackboard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackboard
+{
+    public Transform Target;
+    public float DistanceToTarget;
+    public bool HasLineOfSight;
+
+}
