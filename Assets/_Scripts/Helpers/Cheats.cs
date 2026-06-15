329b3f2af7622dd90830b899a476003890acba15
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2026-06-15 10:20:48 -0300

    fix(cheats): Removed unnecessary cheats

--- Assets/_Scripts/Helpers/Cheats.cs
+++ Assets/_Scripts/Helpers/Cheats.cs
@@ -7,10 +7,6 @@ public class Cheats : MonoBehaviour, IUpdatable
 {
     public int UpdatePriority => Foundation.UpdatePriority.Input;
     
-    [SerializeField] private int _hurtDamage = 10;
-    private readonly string _scene1 = "Core Loop";
-    private readonly string _scene2 = "Hardcore Room";
-    
     private void OnEnable() => UpdateManager.Instance.Register(this);
 
     private void OnDisable() => UpdateManager.Instance?.Unregister(this);
@@ -20,29 +16,6 @@ public class Cheats : MonoBehaviour, IUpdatable
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            var playerHealth = FindObjectOfType<PlayerHealth>();
-            if (playerHealth != null)
-            {
-                playerHealth.TakeDamage(_hurtDamage, ElementType.Neutral);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            if (SceneManager.GetActiveScene().name == _scene1)
-                return;
-            SceneManager.LoadScene(_scene1);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            if (SceneManager.GetActiveScene().name == _scene2)
-                return;
-            SceneManager.LoadScene(_scene2);
         }
     }
 }
