f2d15423cafc9c75cc9feada087d41ea1270da6c
frederickylarcher <federicoivanlarcher@gmail.com>
2026-06-24 21:17:04 -0300

    [FEAT] Finished coding the Pause Menu System
    Implemented:
    
    -- Time-scale handling
    -- Input intercepting
    -- Transition ready layout overlays
    
    Query: the Esc button activates the pause menu, however, the Input Map isn´t binded so that Esc closes the pause menu. Should we move the Pause Game button to a third input map, like a Global one?

--- Assets/_Scripts/5. UI/Menus/PauseMenu.cs
+++ Assets/_Scripts/5. UI/Menus/PauseMenu.cs
@@ -4,18 +4,24 @@ using UnityEngine.SceneManagement;
 
 namespace UI
 {
+    [RequireComponent(typeof(CanvasGroup))]
     public sealed class PauseMenu : MonoBehaviour
     {
-        [Header("Pause Menu")]
+        [Header("Pause Menu Overlays")]
         [SerializeField] private GameObject _pausePanel;
+
+        [Tooltip("Use this to handle overlay visibility and UI input blocking")]
+        [SerializeField] private CanvasGroup _canvasGroup;
 
         private bool _isOpen;
 
         private void Awake()
         {
             _isOpen = false;
-            if (_pausePanel != null)
-                _pausePanel.SetActive(false);
+            ClosePauseMenu();
+
+            if (_canvasGroup != null)
+                _canvasGroup = GetComponent<CanvasGroup>();
 
             Time.timeScale = 1f;
             Helpers.Input.EnablePlayerInput();
@@ -49,8 +55,9 @@ namespace UI
         {
             _isOpen = true;
 
-            if (_pausePanel != null)
-                _pausePanel.SetActive(true);
+            SetOverlayState(true);
+
+            Helpers.Input.EnableUIInput();
 
             Time.timeScale = 0f;
         }
@@ -59,16 +66,31 @@ namespace UI
         {
             _isOpen = false;
 
-            if (_pausePanel != null)
-                _pausePanel.SetActive(false);
+            Helpers.Input.EnablePlayerInput();
+
+            SetOverlayState(false);
 
             Time.timeScale = 1f;
         }
         
+        //Visual and interactive state of the pause menu overlay handler
+        private void SetOverlayState(bool isActive)
+        {
+            if (_pausePanel != null)
+                _pausePanel.SetActive(isActive);
 
+            if (_canvasGroup != null)
+            {
+                _canvasGroup.alpha = isActive ? 1f : 0f;
+                _canvasGroup.blocksRaycasts = isActive;
+                _canvasGroup.interactable = isActive;
+            }
+        }
+
+        // Event Handlers
         private void OnGamePaused(OnGamePausedEvent _) => OpenPauseMenu();
         private void OnGameResumed(OnGameResumedEvent _) => ClosePauseMenu();
-        private void OnRunQuit(OnRunQuitEvent _) => SceneManager.LoadScene(1);
+        private void OnRunQuit(OnRunQuitEvent _) => SceneManager.LoadScene("Main Menu");
     }
 }
 
